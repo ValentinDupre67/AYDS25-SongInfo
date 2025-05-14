a6c71790c7ce8b7c7fa18d7fc13ac5b18b8864fc
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2025-05-14 10:25:11 -0300

    .

---
+++ app/src/main/java/ayds/songinfo/moredetails/fulllogic/presenter/HomePresenter.kt
@@ -0,0 +1,9 @@
+package ayds.songinfo.moredetails.fulllogic.presenter
+
+interface HomePresenter{
+
+}
+
+class HomePresenterImpl(): HomePresenter {
+
+}
---
+++ app/src/main/java/ayds/songinfo/moredetails/fulllogic/view/HomeUiState.kt
@@ -0,0 +1,5 @@
+package ayds.songinfo.moredetails.fulllogic.view
+
+data class HomeUiState(
+    val articleText: String
+)
